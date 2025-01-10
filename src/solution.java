defc8854f860665325ea9f19e02c7c0821b0abb5
chinnu122005 <mallellikusuma@gmail.com>
2025-01-10 11:58:11 +0530

    fifth commit

---
+++ src/solution.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> result = new ArrayList<>();
+       for(int i=0;i<words.length;i++) {
+        String sOne = words[i];
+        for(int j=0;j<words.length;j++) {
+            String sTwo = words[j];
+            if(i != j && sOne.length() < sTwo.length()) {
+                if(isSubstring(sOne,sTwo)==true){
+                    result.add(sOne);
+                    break;
+                }
+            }
+        
+        }
+       }
+    return result;
+}
+public boolean isSubstring(String sOne ,String sTwo){
+    for(int i=0;i<=sTwo.length()-sOne.length();i++){
+        if(sOne.equals(sTwo.substring(i,sOne.length()+i))){
+            return true;
+        }
+
+    }
+    return false;
+
+}
+}
+    
+
